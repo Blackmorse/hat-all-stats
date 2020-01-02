709e5b14bef2acb64e6b165da61dbd70c681ac1b
Blackmorse <blackmorse@live.com>
2020-01-02 22:34:42 +0300

    Loader: project with dependencies

---
+++ loader/src/main/java/com/blackmorse/hattrick/LoaderApplication.java
@@ -0,0 +1,19 @@
+package com.blackmorse.hattrick;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LoaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(ApplicationContext ctx) {
+        return args -> {};
+    }
+}
