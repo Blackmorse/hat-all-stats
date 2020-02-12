a3b8dd81ed6b259264b02b9e2d4988165987eb1b
Blackmorse <blackmorse@live.com>
2020-02-12 15:30:02 +0300

    Loader: fixed leagueUnitIdsLoader

--- loader/src/main/java/com/blackmorse/hattrick/api/LeagueUnitIdsLoader.java
+++ loader/src/main/java/com/blackmorse/hattrick/api/LeagueUnitIdsLoader.java
@@ -52,16 +52,19 @@ public class LeagueUnitIdsLoader {
                 .flatMap(leagueWithLevel -> {
                     log.info("{} league levels loaded", leagueUnitIdCounter.incrementAndGet());
                     if (leagueWithLevel.league.getId() != 1 /* Sweden */) {
-                        List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), Hattrick.arabToRomans.get(leagueWithLevel.getLevel()) + ".1", 0)
-                                .getSearchResults();
+
                         Long baseNumber;
-                        if (leagueWithLevel.getLevel() == 2) {
+                        if (leagueWithLevel.getLevel() == 1) {
+                            List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), "II.1", 0)
+                                    .getSearchResults();
                             baseNumber = searchResults.get(0).getResultId() - 1;
                         } else {
+                            List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), Hattrick.arabToRomans.get(leagueWithLevel.getLevel()) + ".1", 0)
+                                    .getSearchResults();
                             baseNumber = searchResults.get(0).getResultId();
                         }
 
-                        List<LeagueUnitId> leagueUnitIdList = LongStream.range(baseNumber, baseNumber + Hattrick.leagueLevelNumberTeams.get(leagueWithLevel.getLevel()) + 1)
+                        List<LeagueUnitId> leagueUnitIdList = LongStream.range(baseNumber, baseNumber + Hattrick.leagueLevelNumberTeams.get(leagueWithLevel.getLevel()))
                                 .mapToObj(number -> LeagueUnitId.builder()
                                         .league(leagueWithLevel.league)
                                         .id(number)
@@ -71,10 +74,14 @@ public class LeagueUnitIdsLoader {
                         return Flowable.fromIterable(leagueUnitIdList);
                     } else {
                         Long baseNumber;
-                        if (leagueWithLevel.getLevel() == 2) {
+                        if (leagueWithLevel.getLevel() == 1) {
                             List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), "Ia", 0)
                                     .getSearchResults();
                             baseNumber = searchResults.get(0).getResultId() - 1;
+                        } else if (leagueWithLevel.getLevel() == 2) {
+                            List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), "Ia", 0)
+                                    .getSearchResults();
+                            baseNumber = searchResults.get(0).getResultId();
                         } else if (leagueWithLevel.getLevel() == 3) {
                             List<Result> searchResults = hattrick.searchLeagueUnits(leagueWithLevel.getLeague().getId(), "IIa", 0)
                                     .getSearchResults();
