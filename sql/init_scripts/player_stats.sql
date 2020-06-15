346965d3f0360c48ed1212fbc30645566bd9f338
blackmorse <blackmorse@live.com>
2020-06-15 22:32:58 +0300

    #59 Find place for round at primary key (match_details, player_stats)

--- sql/init_scripts/player_stats.sql
+++ sql/init_scripts/player_stats.sql
@@ -1,33 +1,33 @@
 CREATE TABLE player_stats
 (
-    `season` UInt8, 
-    `league_id` UInt16, 
-    `division_level` UInt8, 
-    `league_unit_id` UInt32, 
-    `league_unit_name` LowCardinality(String), 
-    `team_id` UInt64, 
-    `team_name` String, 
-    `time` DateTime, 
-    `dt` Date DEFAULT toDate(time), 
-    `round` UInt8, 
-    `match_id` UInt64, 
-    `player_id` UInt64, 
-    `first_name` String, 
-    `last_name` String, 
-    `age` UInt8, 
-    `days` UInt8, 
-    `role_id` UInt8, 
-    `played_minutes` UInt8, 
-    `rating` UInt8, 
-    `rating_end_of_match` UInt8, 
-    `injury_level` Int8, 
-    `tsi` UInt32, 
-    `salary` UInt32, 
-    `yellow_cards` UInt8, 
-    `red_cards` UInt8, 
+    `season` UInt8,
+    `league_id` UInt16,
+    `division_level` UInt8,
+    `league_unit_id` UInt32,
+    `league_unit_name` String,
+    `team_id` UInt64,
+    `team_name` String,
+    `time` DateTime,
+    `dt` Date DEFAULT toDate(time),
+    `round` UInt8,
+    `match_id` UInt64,
+    `player_id` UInt64,
+    `first_name` String,
+    `last_name` String,
+    `age` UInt8,
+    `days` UInt8,
+    `role_id` UInt8,
+    `played_minutes` UInt8,
+    `rating` UInt8,
+    `rating_end_of_match` UInt8,
+    `injury_level` Int8,
+    `tsi` UInt32,
+    `salary` UInt32,
+    `yellow_cards` UInt8,
+    `red_cards` UInt8,
     `goals` UInt8
 )
-ENGINE = ReplacingMergeTree()
+ENGINE = MergeTree()
 PARTITION BY season
-ORDER BY (season, league_id, division_level, league_unit_id, team_id, round, cityHash64(player_id))
-SETTINGS index_granularity = 512;
+ORDER BY (season, league_id, division_level, league_unit_id, round)
+SETTINGS index_granularity = 4096
