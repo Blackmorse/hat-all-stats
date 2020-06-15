994e5e2264ffbadbd7c1868a41b234b74a617d5d
blackmorse <blackmorse@live.com>
2020-06-15 20:37:52 +0300

    Rankings job for loader

--- loader/src/main/java/com/blackmorse/hattrick/LoaderApplication.java
+++ loader/src/main/java/com/blackmorse/hattrick/LoaderApplication.java
@@ -1,5 +1,7 @@
 package com.blackmorse.hattrick;
 
+import com.blackmorse.hattrick.api.Hattrick;
+import com.blackmorse.hattrick.clickhouse.TeamRankJoiner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +13,7 @@ import org.springframework.retry.annotation.EnableRetry;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -37,6 +40,20 @@ public class LoaderApplication {
                 List<String> countriesList = Arrays.stream(countries.split(",")).map(String::trim).collect(Collectors.toList());
                 log.info("Command to load countries: {}", countriesList);
                 ctx.getBean(CountriesLastLeagueMatchLoader.class).load(countriesList);
+            } else if (args[0].equals("rankJoin")) {
+                TeamRankJoiner rankJoiner = ctx.getBean(TeamRankJoiner.class);
+                AtomicInteger counter = new AtomicInteger();
+                ctx.getBean(Hattrick.class).getWorldDetails().getLeagueList().forEach(league -> {
+                    counter.incrementAndGet();
+                    for (int round = 1; round <= 2; round ++) {
+                        log.info("{}. Joining all leagues for ({}, {}), round {}", counter.get(), league.getLeagueName(), league.getLeagueId(), round);
+                        rankJoiner.join(75, league.getLeagueId(), round, null);
+                        for (int divisionLevel = 1; divisionLevel <= league.getNumberOfLevels(); divisionLevel ++) {
+                            log.info("{}. Joining {} division for ({}, {}), round {}", counter.get(), divisionLevel, league.getLeagueName(), league.getLeagueId(), round);
+                            rankJoiner.join(75, league.getLeagueId(), round, divisionLevel);
+                        }
+                    }
+                });
             }
         };
     }
