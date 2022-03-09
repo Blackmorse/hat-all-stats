d66ff0e5ed43bc21bfefe2135aa0448704619582
blackmorse <blackmorse@live.com>
2022-03-09 22:45:16 +0300

    Quick fix for CHPP bug: up leaguedetails file version

--- chpp/src/main/scala/chpp/leaguedetails/LeagueDetailsRequest.scala
+++ chpp/src/main/scala/chpp/leaguedetails/LeagueDetailsRequest.scala
@@ -6,7 +6,7 @@ import chpp.{AbstractRequest, OauthTokens, RequestCreator}
 
 case class LeagueDetailsRequest(leagueUnitId: Option[Int]) extends AbstractRequest[LeagueDetails] {
   override def createRequest()(implicit oauthTokens: OauthTokens): HttpRequest = {
-    val map = RequestCreator.params("leaguedetails", "1.5",
+    val map = RequestCreator.params("leaguedetails", "1.6",
       "leagueLevelUnitID" -> leagueUnitId)
 
     RequestCreator.create(map)
