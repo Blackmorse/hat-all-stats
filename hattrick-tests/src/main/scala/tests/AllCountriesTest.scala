aad71f30bb2f6fd8668cb7806ac6afd424414df8
Blackmorse <blackmorse@live.com>
2022-06-22 19:23:42 +0400

    #109 tests: Make tests for round consistency

--- hattrick-tests/src/main/scala/tests/AllCountriesTest.scala
+++ hattrick-tests/src/main/scala/tests/AllCountriesTest.scala
@@ -1,60 +1,19 @@
 package tests
 
-import akka.actor.ActorSystem
-import chpp.worlddetails.WorldDetailsRequest
 import chpp.worlddetails.models.WorldDetails
-import chpp.{ChppRequestExecutor, OauthTokens}
-import com.typesafe.config.ConfigFactory
-import hattid.CupSchedule.isSummerTimeNow
-import hattid.telegram.TelegramClient
-import hattid.telegram.TelegramClient.TelegramCreds
 import hattid.{CommonData, CupSchedule, ScheduleEntry}
 
 import java.util.{Calendar, Date}
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 object AllCountriesTest {
 
-  def main(args: Array[String]): Unit = {
-    implicit val actorSystem: ActorSystem = ActorSystem("TestsActorSystem")
-
-    val config = ConfigFactory.load()
-
-    val authToken = config.getString("tokens.authToken")
-    val authCustomerKey = config.getString("tokens.authCustomerKey")
-    val clientSecret = config.getString("tokens.clientSecret")
-    val tokenSecret = config.getString("tokens.tokenSecret")
-
-    val botToken = config.getString("telegram.botToken")
-    val chatId = config.getString("telegram.chatId")
-
-    implicit val oauthTokens: OauthTokens = OauthTokens(authToken, authCustomerKey, clientSecret, tokenSecret)
-    implicit val telegramCreds: TelegramCreds = TelegramCreds(chatId = chatId, botToken = botToken)
-
-    val worldDetails = Await.result(ChppRequestExecutor.execute(WorldDetailsRequest()), 1.minute)
-
-    try {
-      testNumberOfCountries(worldDetails)
-      testFirstAndLastLeague(worldDetails)
-      testDivisionLevels(worldDetails)
-      testCupSchedule(worldDetails)
-      Await.result(TelegramClient.sendMessage("Countries tests succesfully passed!"), 1.minute)
-    } catch {
-      case e: Exception =>
-        Await.result(TelegramClient.sendMessage(e.getMessage), 1.minute)
-    }
-
-    actorSystem.terminate()
-  }
-
-  private def testNumberOfCountries(worldDetails: WorldDetails): Unit = {
+  def testNumberOfCountries(worldDetails: WorldDetails): Unit = {
     if (CommonData.higherLeagueMap.size != worldDetails.leagueList.size) {
       throw new Exception(s"Expected ${CommonData.higherLeagueMap.size} countries, but got ${worldDetails.leagueList.size}")
     }
   }
 
-  private def testCupSchedule(worldDetails: WorldDetails): Unit = {
+  def testCupSchedule(worldDetails: WorldDetails): Unit = {
 
     val schedule = CupSchedule.normalizeCupScheduleToDayOfWeek(CupSchedule.seq, Calendar.MONDAY)
       .sortBy(_.leagueId)
@@ -77,7 +36,7 @@ object AllCountriesTest {
     }
   }
 
-  private def testFirstAndLastLeague(worldDetails: WorldDetails): Unit = {
+  def testFirstAndLastLeague(worldDetails: WorldDetails): Unit = {
     val schedule = CupSchedule.normalizeCupScheduleToDayOfWeek(worldDetails.leagueList
       .map(league => ScheduleEntry(league.leagueId, league.seriesMatchDate)), Calendar.THURSDAY)
       .sortBy(_.date)
@@ -90,9 +49,9 @@ object AllCountriesTest {
     }
   }
 
-  private def testDivisionLevels(worldDetails: WorldDetails): Unit = {
+  def testDivisionLevels(worldDetails: WorldDetails): Unit = {
     if (worldDetails.leagueList.exists(_.numberOfLevels > 10)) {
-      throw new Exception(s"There is a leagues with more than 11 divisionLevels: " +
+      throw new Exception(s"There is a leagues with more than 10 divisionLevels: " +
         s"${worldDetails.leagueList.filter(_.numberOfLevels > 10).map(_.leagueId).mkString(",")}")
     }
   }
--- sql/init_scripts/10-insert-data.sh
+++ sql/init_scripts/10-insert-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a tables=("match_details" "player_stats" "team_details" "team_rankings" "promotions")
+declare -a tables=("match_details" "player_stats" "team_details" "team_rankings")
 
 for table in "${tables[@]}"; do
   cat $(dirname "$0")/sample_data/$table.csv | clickhouse-client --query "insert into hattrick.$table format CSV"
