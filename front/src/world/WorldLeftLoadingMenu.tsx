126adf358a9736fef06182bf384663cf54c32682
Andrei <blackmorse@live.com>
2022-10-03 22:13:11 +0400

    Bug: front: no total world countries at loading menu

--- front/src/world/WorldLeftLoadingMenu.tsx
+++ front/src/world/WorldLeftLoadingMenu.tsx
@@ -11,39 +11,37 @@ interface Props {
     worldLevelDataProps?: WorldLevelDataProps
 }
 
-class WorldLeftLoadingMenu extends React.Component<Props, {}> {
-    render() {
-        if(!this.props.worldLevelDataProps || !this.props.worldLevelDataProps.loadingInfo()) {
-            return <></>
-        }
-        let worldData = this.props.worldLevelDataProps
-        let percent = worldData.loadingInfo().proceedCountries / worldData.countries.length
-
-        let currentProcess: JSX.Element
-        if(worldData.loadingInfo().currentCountry) {
-        currentProcess = <span><LeagueLink text={worldData.loadingInfo().currentCountry![1]} id={worldData.loadingInfo().currentCountry![0]} />{i18n.t('world.loading')}</span>
-        } else if (worldData.loadingInfo().nextCountry) {
-            currentProcess = <span className="h6">
-                    <LeagueLink className="left-menu-link link-dark p-0" text={worldData.loadingInfo().nextCountry![1]} id={worldData.loadingInfo().nextCountry![0]} />{i18n.t('world.scheduled')}<br/>
-                    {moment(worldData.loadingInfo().nextCountry![2]).format('DD.MM HH:mm:ss')} HT
-                </span>
-        }
-
-        return <Translation>{
-            t =>  
-            <Card className="shadow">
-                <Card.Header className='lead'>Progress</Card.Header>
-                <Card.Body>
-                    {worldData.loadingInfo().proceedCountries} / {worldData.countries.length} {t('world.loaded')}
-                    <ProgressBar now={percent * 100}/> 
-                    {currentProcess}
-                </Card.Body>
-            </Card>
-
-
-        }
-        </Translation>
+const WorldLeftLoadingMenu = (props: Props) => {
+    if(!props.worldLevelDataProps || !props.worldLevelDataProps.loadingInfo()) {
+        return <></>
     }
+    let worldData = props.worldLevelDataProps
+    let percent = worldData.loadingInfo().proceedCountries / worldData.countries().length
+
+    let currentProcess: JSX.Element
+    if(worldData.loadingInfo().currentCountry) {
+    currentProcess = <span><LeagueLink text={worldData.loadingInfo().currentCountry![1]} id={worldData.loadingInfo().currentCountry![0]} />{i18n.t('world.loading')}</span>
+    } else if (worldData.loadingInfo().nextCountry) {
+        currentProcess = <span className="h6">
+                <LeagueLink className="left-menu-link link-dark p-0" text={worldData.loadingInfo().nextCountry![1]} id={worldData.loadingInfo().nextCountry![0]} />{i18n.t('world.scheduled')}<br/>
+                {moment(worldData.loadingInfo().nextCountry![2]).format('DD.MM HH:mm:ss')} HT
+            </span>
+    }
+
+    return <Translation>{
+        t =>  
+        <Card className="shadow">
+            <Card.Header className='lead'>Progress</Card.Header>
+            <Card.Body>
+                {worldData.loadingInfo().proceedCountries} / {worldData.countries().length} {t('world.loaded')}
+                <ProgressBar now={percent * 100}/> 
+                {currentProcess}
+            </Card.Body>
+        </Card>
+
+
+    }
+    </Translation>
 }
 
 export default WorldLeftLoadingMenu
