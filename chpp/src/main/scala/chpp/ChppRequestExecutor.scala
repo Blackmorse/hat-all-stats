18b8f3725ed8f950a3e12be8a11aea507a28862d
Andrei.Kochemirovskii <blackmorse@live.com>
2022-12-10 11:04:10 +0400

    Fix Workaround: chppclient retries

--- chpp/src/main/scala/chpp/ChppRequestExecutor.scala
+++ chpp/src/main/scala/chpp/ChppRequestExecutor.scala
@@ -9,13 +9,13 @@ import scala.concurrent.duration._
 import scala.xml.XML
 
 object ChppRequestExecutor {
-  private val retries = 3
+  private val retries = 6
   def execute[Model](request: AbstractRequest[Model], retry: Int = 0)
                     (implicit oauthTokens: OauthTokens, system: ActorSystem, reader: XmlReader[Model]): Future[Model] = {
     import system.dispatcher
 
-    for (response <- Http().singleRequest(request.createRequest());
-         responseBody <- response.entity.toStrict(1.minute)) yield {
+    val v = for (response <- Http().singleRequest(request.createRequest());
+         responseBody <- response.entity.toStrict(3.minute)) yield {
       val string = responseBody.data.utf8String
       val preprocessed = request.preprocessBody(string)
       val xml = XML.loadString(preprocessed)
@@ -23,12 +23,14 @@ object ChppRequestExecutor {
       val modelParse = XmlReader.of[Model].read(xml)
 
       if (modelParse.errors.nonEmpty && retry <= retries) {
-        Await.result(execute(request, retry + 1), ((retry) * 4).seconds)
+        execute(request, retry + 1)
       } else if (modelParse.errors.nonEmpty) {
         throw new Exception(s"It was for request $request. And response was: \n $string \nParse model have an errors: ${modelParse.errors.map(_.toString).mkString("[", ",", "]")}")
       } else {
-        modelParse.getOrElse(throw new Exception("Unable to parse"))
+        modelParse.map(m => Future(m)).getOrElse(throw new Exception("Unable to parse"))
       }
     }
+
+    v.flatten
   }
 }
