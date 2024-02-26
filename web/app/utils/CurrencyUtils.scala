dac46a5f39eb640da7bc67ccd80c2c15e7d63662
Andrei.Kochemirovskii <blackmorse@live.com>
2024-02-26 23:02:37 +0400

    web: trigger build

--- web/app/utils/CurrencyUtils.scala
+++ web/app/utils/CurrencyUtils.scala
@@ -6,6 +6,7 @@ object CurrencyUtils {
   def currencyRate(country: Option[Country]): Double =
     country.map(_.currencyRate).getOrElse(10.0)
 
+
   def currencyName(country: Option[Country]): String =
     country.map(_.currencyName).getOrElse("$")
 }
