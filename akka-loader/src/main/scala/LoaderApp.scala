d36c9c3a95b631ffdc58464d0ae6c32000b0b067
Blackmorse <blackmorse@live.com>
2024-03-26 11:47:41 +0400

    akka-loader: fix terminating actorSystem

--- akka-loader/src/main/scala/LoaderApp.scala
+++ akka-loader/src/main/scala/LoaderApp.scala
@@ -19,60 +19,63 @@ import scala.concurrent.duration._
 
 
 object LoaderApp extends App {
-  val cliConfig = new CommandLine(args).toCliConfig
-
   private val logger = Logger(LoggerFactory.getLogger(this.getClass))
 
   implicit val actorSystem: ActorSystem = ActorSystem("LoaderActorSystem")
   import actorSystem.dispatcher
 
-  val config = ConfigFactory.load()
+  try {
+    val cliConfig = new CommandLine(args).toCliConfig
+    val config = ConfigFactory.load()
 
-  val injector = Guice.createInjector(new LoaderModule(config, actorSystem))
+    val injector = Guice.createInjector(new LoaderModule(config, actorSystem))
 
-  implicit val oauthTokens: OauthTokens = injector.getInstance(classOf[OauthTokens])
+    implicit val oauthTokens: OauthTokens = injector.getInstance(classOf[OauthTokens])
 
-  val executorActorFactory: ExecutorActorFactory = injector
+    val executorActorFactory: ExecutorActorFactory = injector
       .getInstance(classOf[ExecutorActorFactory])
 
-  val worldDetailsFuture = WorldDetailsSingleRequest.request(leagueId = None)
+    val worldDetailsFuture = WorldDetailsSingleRequest.request(leagueId = None)
 
-  private val worldDetails = Await.result(worldDetailsFuture, 30.seconds)
+    val worldDetails = Await.result(worldDetailsFuture, 30.seconds)
 
-  cliConfig match {
-    case ScheduleConfig(fromOpt, entity, lastMatchWindow) =>
-      val (taskExecutorActor, scheduler) = executorAndScheduler(entity, lastMatchWindow, executorActorFactory, worldDetails)
-      fromOpt match {
-        case Some(from) => scheduler.scheduleFrom(from)
-        case None => scheduler.schedule()
-      }
-      actorSystem.scheduler.scheduleWithFixedDelay(0.second , 5.second)(() => taskExecutorActor ! TryToExecute)
-    case LoadConfig(leagues, entity, lastMatchWindow) =>
-      val (taskExecutorActor, scheduler) = executorAndScheduler(entity, lastMatchWindow, executorActorFactory, worldDetails)
-      scheduler.load(leagues)
-      actorSystem.scheduler.scheduleWithFixedDelay(0.second , 5.second)(() => taskExecutorActor ! TryToExecute)
-    case TeamRankingsConfig(Some(league)) =>
-      Await.result(TeamRankJoiner.joinTeamRankings(config, worldDetails.leagueList.find(_.leagueName == league).get), 3.minute)
-    case TeamRankingsConfig(None) =>
-      worldDetails.leagueList.foreach(league => {
-        Await.result(TeamRankJoiner.joinTeamRankings(config, league), 3.minute)
-      })
-    case LoadScheduledConfig(entity, lastMatchWindow) =>
-      val realEntity = if (entity == "auto") {
-        val c = Calendar.getInstance()
-        val dayOfWeek = c.get(Calendar.DAY_OF_WEEK)
-        if (Set(Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY, Calendar.MONDAY).contains(dayOfWeek)) {
-          "league"
-        } else {
-          "cup"
+    cliConfig match {
+      case ScheduleConfig(fromOpt, entity, lastMatchWindow) =>
+        val (taskExecutorActor, scheduler) = executorAndScheduler(entity, lastMatchWindow, executorActorFactory, worldDetails)
+        fromOpt match {
+          case Some(from) => scheduler.scheduleFrom(from)
+          case None => scheduler.schedule()
         }
-      } else {
-        entity
-      }
-      logger.info(s"Entity type: $entity")
-      val (taskExecutorActor, scheduler) = executorAndScheduler(realEntity, lastMatchWindow, executorActorFactory, worldDetails)
-      scheduler.loadScheduled()
-      actorSystem.scheduler.scheduleWithFixedDelay(0.second , 5.second)(() => taskExecutorActor ! TryToExecute)
+        actorSystem.scheduler.scheduleWithFixedDelay(0.second, 5.second)(() => taskExecutorActor ! TryToExecute)
+      case LoadConfig(leagues, entity, lastMatchWindow) =>
+        val (taskExecutorActor, scheduler) = executorAndScheduler(entity, lastMatchWindow, executorActorFactory, worldDetails)
+        scheduler.load(leagues)
+        actorSystem.scheduler.scheduleWithFixedDelay(0.second, 5.second)(() => taskExecutorActor ! TryToExecute)
+      case TeamRankingsConfig(Some(league)) =>
+        Await.result(TeamRankJoiner.joinTeamRankings(config, worldDetails.leagueList.find(_.leagueName == league).get), 3.minute)
+      case TeamRankingsConfig(None) =>
+        worldDetails.leagueList.foreach(league => {
+          Await.result(TeamRankJoiner.joinTeamRankings(config, league), 3.minute)
+        })
+      case LoadScheduledConfig(entity, lastMatchWindow) =>
+        val realEntity = if (entity == "auto") {
+          val c = Calendar.getInstance()
+          val dayOfWeek = c.get(Calendar.DAY_OF_WEEK)
+          if (Set(Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY, Calendar.MONDAY).contains(dayOfWeek)) {
+            "league"
+          } else {
+            "cup"
+          }
+        } else {
+          entity
+        }
+        logger.info(s"Entity type: $entity")
+        val (taskExecutorActor, scheduler) = executorAndScheduler(realEntity, lastMatchWindow, executorActorFactory, worldDetails)
+        scheduler.loadScheduled()
+        actorSystem.scheduler.scheduleWithFixedDelay(0.second, 5.second)(() => taskExecutorActor ! TryToExecute)
+    }
+  } finally {
+    actorSystem.terminate()
   }
 
   private def executorAndScheduler(entity: String, lastMatchesWindow: Int, executorActorFactory: ExecutorActorFactory, worldDetails: WorldDetails): (ActorRef, AbstractScheduler) = {
